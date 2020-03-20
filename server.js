55d577f6ab3772ec15f2e7d8e1e8b6a008cd44f8
Alison Law <alisparklaw@gmail.com>
2020-03-20 11:13:41 +0000

    post task  working from postman and sql

--- server.js
+++ server.js
@@ -71,4 +71,18 @@ server.post("/resources", async (req, res) => {
   }
 });
 
+function insertTask(task){
+  //INSERT INTO resources(resource_description,resource_name) values ('scissors raw sql', 'raw');
+   return db("tasks").insert(task);
+ }
+ server.post("/tasks", async (req, res) => {
+   try {
+     const data = await insertTask(req.body);
+     db("tasks").insert(req.body);
+     res.status(200).json({ data });
+   } catch (err) {
+     console.log(err);
+     res.status(500).json({ message: "Fatal error posting new task" });
+   }
+ });
 module.exports = server;
