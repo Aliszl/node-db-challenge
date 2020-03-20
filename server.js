2fb4a1d7497c70adb498501ea1def8e0cf040f54
Alison Law <alisparklaw@gmail.com>
2020-03-20 10:37:48 +0000

    post project working from postman and sql

--- server.js
+++ server.js
@@ -41,6 +41,25 @@ server.get('/tasks', (req, res) => {
   });
 });
 
+function insert(project) {
+  //INSERT INTO projects(project_description,project_name) 
+  //values ('Batch cook for week ahead raw sql', 'batch cook');
+  return db("projects")
+    .insert(project)
+}
+server.post('/projects', async(req, res) => {
+  try{
+    const data = await insert(req.body);
+    db('projects').insert(req.body)
+    res.status(200).json({ data });
+  }catch(err){
+    console.log(err);
+    res.status(500).json({ message: "Fatal error posting new project" });
+  }
+  
+});
+
+
 
 
 module.exports = server;
